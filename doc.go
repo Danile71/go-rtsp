27e0e4baa81c305803de545256600a0fd019c9f5
Daniel <danile71@gmail.com>
2021-07-23 16:34:50 +0300

    Add docs (#4)
    * fix rn (#1)
    
    * add docs (#3)
    
    * fix date in LICENSE

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package go-rtsp is a Golang audio/video library based on FFmpeg.
+// go-rtsp is powerful library to help convert streams/files to jpeg/wav.
+package rtsp
--- pkt.go
+++ pkt.go
@@ -3,57 +3,68 @@ package rtsp
 // #include "ffmpeg.h"
 import "C"
 
-type Packet struct {
-	streamIndex int
-	codecType   int
-	data        []byte
-
+type Image struct {
 	// only image
-	width  int
-	height int
+	width    int
+	height   int
+	keyFrame bool
+}
 
+// Height if image or 0 if audio
+func (image *Image) Height() int {
+	return image.height
+}
+
+// Width if image or 0 if audio
+func (image *Image) Width() int {
+	return image.width
+}
+
+type Audio struct {
 	// only audio
 	sampleRate int
 	bitRate    int
 	channels   int
 }
 
-// Height if image or 0 if audio
-func (pkt *Packet) Height() int {
-	return pkt.height
-}
-
-// Width if image or 0 if audio
-func (pkt *Packet) Width() int {
-	return pkt.width
-}
-
 // SampleRate if audio or 0 if image
-func (pkt *Packet) SampleRate() int {
-	return pkt.sampleRate
+func (audio *Audio) SampleRate() int {
+	return audio.sampleRate
 }
 
 // BitRate if audio or 0 if image
-func (pkt *Packet) BitRate() int {
-	return pkt.bitRate
+func (audio *Audio) BitRate() int {
+	return audio.bitRate
 }
 
-// BitRate if audio or 0 if image
-func (pkt *Packet) Channels() int {
-	return pkt.channels
+// Channels if audio or 0 if image
+func (audio *Audio) Channels() int {
+	return audio.channels
+}
+
+type Packet struct {
+	streamIndex int
+	codecType   int
+	data        []byte
+
+	duration int64
+	position int64
+
+	Image
+	Audio
 }
 
 // Data encoded jpeg if image or wav if audio
-func (pkt *Packet) Data() []byte {
-	return pkt.data
+func (packet *Packet) Data() []byte {
+	return packet.data
 }
 
 // IsAudio packet
-func (pkt *Packet) IsAudio() bool {
-	return pkt.codecType == C.AVMEDIA_TYPE_AUDIO
+func (packet *Packet) IsAudio() bool {
+	return packet.codecType == C.AVMEDIA_TYPE_AUDIO
 }
 
 // IsVideo packet
-func (pkt *Packet) IsVideo() bool {
-	return pkt.codecType == C.AVMEDIA_TYPE_VIDEO
+func (packet *Packet) IsVideo() bool {
+	return packet.codecType == C.AVMEDIA_TYPE_VIDEO
 }
